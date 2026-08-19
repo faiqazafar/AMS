ff75f57da411a8952273419095da3bb67c85b1e7
faiqazafar <zibal760@gmail.com>
2026-08-20 00:32:36 +0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,19 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "itams");
+
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
+
+$conn = mysqli_connect(
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+);
+
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Database connection failed: " . mysqli_connect_error());
 }
-?>
